729a1e8c4ce6e9cfaf23d4619f4322709b62f9b6
ryan <ryan@sunyuki.com>
2016-12-31 03:27:52 +0800

    upload 4s code

---
+++ code/4s.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+typedef struct {
+	
+	int id;
+	char* name;
+	char* reg;
+	int numOfAddrs;
+	char* addrs[];
+} account;
+
+//
+int listMatchAccounts(char* addr, account allAccounts[], int numOfAccounts)
+{
+	int matchCount = 0;
+	for (int i = 0; i < numOfAccounts; ++i)
+	{
+		if (allAccounts[i].numOfAddrs > 0 && allAccounts[i].addrs[0] == addr)
+		{
+			printf("name:%s, address:%s\n", allAccounts[i].name, allAccounts[i].addrs[0]);
+			matchCount++;
+		}
+	}
+	return matchCount;
+};
+
+//test
+int main(int argc, char const *argv[])
+{
+	account data[3] = {{1,"ryan","AECCS",1},{2,"jack","ESCSA",1},{1,"helen","JKDDC",1}};
+	data[0].addrs[0] = "chengdu";
+	data[0].numOfAddrs = 1;
+	data[1].addrs[0] = "chengdu";
+	data[1].numOfAddrs = 1;
+	data[2].addrs[0] = "shanghai";
+	data[2].numOfAddrs = 1;
+
+
+	printf("total records:%d\n", listMatchAccounts("chengdu", data, 3));
+ 
+	return 0;
+};
+
